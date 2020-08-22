e8372322f0a4f391fa73b791aa43e753ae6555c0
arjunp <>
2020-08-22 13:07:09 +0530

    imcluded email in search option

--- app/src/main/java/com/quastio/employeedirectory/db/EmployeeDao.kt
+++ app/src/main/java/com/quastio/employeedirectory/db/EmployeeDao.kt
@@ -14,7 +14,8 @@ interface EmployeeDao {
     @Query("SELECT * FROM employee")
     fun getAllEmployees(): List<EmployeeDbModel>
 
-    @Query("SELECT * FROM employee WHERE name LIKE :key")
+    @Query("SELECT * FROM employee WHERE name LIKE :key " +
+            "OR email LIKE :key")
     suspend fun getFilterEmployee(key:String): List<EmployeeDbModel>
 
 }
