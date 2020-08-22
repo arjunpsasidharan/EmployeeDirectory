4d061b5f67765da3126f9f8602e27194846361c6
arjunp <>
2020-08-22 10:22:45 +0530

    added structure and dependencies

--- app/src/main/java/com/quastio/employeedirectory/views/MainActivity.kt
+++ app/src/main/java/com/quastio/employeedirectory/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.quastio.employeedirectory
+package com.quastio.employeedirectory.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.quastio.employeedirectory.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
